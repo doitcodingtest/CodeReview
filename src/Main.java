141d526182f626d9bf07fe6e3f7981c850e8178f
todayCoding <codingtest@naver.com>
2024-08-30 12:52:56 +0900

    ProductMgmtSystem 클래스 추가 - 기존 Main에서 관리하던 상품을 관리하는 용도 - totalPriceForProducts 함수 이동 (Main -> ProductMgmtSystem) - addProduct 함수 신규 생성

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,10 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static ArrayList<Product>products = new ArrayList<>();
+    static ProductMgmtSystem productMgmtSystem = new ProductMgmtSystem();
     public static void main(String[] args) {
-        products.add(new Product("과자", 1000));
-        products.add(new Product("사탕", 500));
-        System.out.println(totalPriceForProducts());
-    }
-
-    public static int totalPriceForProducts(){
-        int totalPrice = 0;
-        for(Product product: products){
-            totalPrice += product.getPrice();
-        }
-        return totalPrice;
+        productMgmtSystem.addProduct(new Product("과자", 1000));
+        productMgmtSystem.addProduct(new Product("사탕", 500));
+        System.out.println(productMgmtSystem.totalPriceForProducts());
     }
 }
