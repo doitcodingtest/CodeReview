dc6a0e8bf829c5715ee83d0e878fd493d9339426
todayCoding <codingtest@naver.com>
2024-08-30 12:40:17 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    ArrayList<Product>products = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public int totalPriceForProducts(){
+        int totalPrice = 0;
+        for(Product product: products){
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
     }
 }
